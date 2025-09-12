a49c20b5d1d9552437a52795794c19cde7b080d1
Renzoh17 <hirschfeldrenzo@gmail.com>
2025-09-12 18:50:37 -0300

    Create WebRestApp

---
+++ App-ASPNET-/WebRestApp-ASPNET-/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebRestApp_ASPNET_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
